8158e00d0bed6d2ceef14a96c7216fdb75a916dd
Gian <gia.munozs@duocuc.cl>
2025-05-30 16:58:19 -0400

    j

--- sis_inventario/src/store/UsuariosStore.jsx
+++ sis_inventario/src/store/UsuariosStore.jsx
@@ -5,7 +5,7 @@ export const useUsuarioStore = create((set,get)=>({
     insertarUsuarioAdmin: async(p)=>{
         await supabase.auth.signUp({
             email: p.correo,
-            password: p.pass,
+            password: p.pass
             });
     }
     f
